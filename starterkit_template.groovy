a556f5278471fc0953a4f6f642c1f6f7f4d22c4d
Jinbo Wang <jinbwan@microsoft.com>
2016-09-28 11:06:42 +0800

    fix

--- starterkit_template.groovy
+++ starterkit_template.groovy
@@ -7,7 +7,6 @@ def settings = new JsonSlurper().parseText(jsonPayloadJobs)
 def parseRepoName(repoUrl) {
     def parts = repoUrl.split(/[\/\\]/)
     def repoName = parts[-1]
-    repoName = repoName.replaceAll(/\./, '_')
     return repoName
 }
 
@@ -40,43 +39,8 @@ def createJob(jobName, upstreamJobName, jobSettings, codeBranch, buildSteps) {
     }
 }
 
-def createOpbuildJob(jobName, upstreamJobName, jobSettings, testBranch) {
-    def contentBranch = jobSettings['branchMapping'][testBranch]['contentBranch']
-    def opbuildEndpoint = jobSettings['branchMapping'][testBranch]['opbuildEnv']
-    job(jobName) {
-        label(jobSettings['labelExpression'])
-        logRotator { // Discard old builds
-            daysToKeep(14) // If specified, build records are only kept up to this number of days.
-            numToKeep(40) // If specified, only up to this number of build records are kept.
-            artifactDaysToKeep(14) // If specified, artifacts from builds older than this number of days will be deleted, but the logs, history, reports, etc for the build will be kept.
-            artifactNumToKeep(40) // If specified, only up to this number of builds have their artifacts retained.
-        }
-        triggers {
-            upstream(upstreamJobName, 'SUCCESS')
-        }
-        scm {
-            git {
-                remote {
-                    url(jobSettings['repoUrl'])
-                    credentials(jobSettings['credentialId'])
-                }
-                branch('develop')
-            }
-        }
-        steps {
-            shell([
-                'npm install && npm update',
-                "npm run opse2e -- mergeE2eTestData --sourceBranch master --targetBranch ${contentBranch}",
-                "npm run opse2e -- publishE2eTestData --buildEndpoint ${opbuildEndpoint} --buildBranch ${contentBranch}"
-                ].join('\n'))
-        }
-    }
-}
-
 def createE2eJob(jobName, upstreamJobName, jobSettings, testBranch) {
     def e2eSettings = jobSettings['branchMapping'][testBranch]
-    def commonE2eRepoName = parseRepoName(jobSettings['commonE2eRepoUrl'])
-    def repoName = parseRepoName(jobSettings['repoUrl'])
     job(jobName) {
         label(jobSettings['labelExpression'])
         logRotator { // Discard old builds
@@ -88,46 +52,40 @@ def createE2eJob(jobName, upstreamJobName, jobSettings, testBranch) {
         triggers {
             upstream(upstreamJobName, 'SUCCESS')
         }
-        multiscm {
-            git {
-                remote {
-                    url(jobSettings['commonE2eRepoUrl'])
-                    credentials(jobSettings['credentialId'])
-                }
-                branch(e2eSettings['sourceBranch'])
-                extensions {
-                    relativeTargetDirectory(commonE2eRepoName)
+        if (jobSettings['repoUrl'].class == String) {
+            scm {
+                git {
+                    remote {
+                        url(jobSettings['repoUrl'])
+                        credentials(jobSettings['credentialId'])
+                    }
+                    branch(e2eSettings['sourceBranch'])
                 }
             }
-            git {
-                remote {
-                    url(jobSettings['repoUrl'])
-                    credentials(jobSettings['credentialId'])
-                }
-                branch(e2eSettings['sourceBranch'])
-                extensions {
-                    relativeTargetDirectory(repoName)
+        } else {
+            multiscm {
+                jobSettings['repoUrl'].each { repoUrl ->
+                    git {
+                        remote {
+                            url(repoUrl)
+                            credentials(jobSettings['credentialId'])
+                        }
+                        branch(e2eSettings['sourceBranch'])
+                        extensions {
+                            relativeTargetDirectory(parseRepoName(repoUrl))
+                        }
+                    }
                 }
             }
         }
         steps {
-            shell([
-                "cd ${commonE2eRepoName}",
-                "npm install && npm update",
-                "npm run opse2e -- init",
-                "npm run opse2e -- protractor --configFile ${e2eSettings['configFile']} --baseUrl ${e2eSettings['baseUrl']} --branchName ${e2eSettings['contentBranch']} --noEscalation",
-                "",
-                "cd ../${repoName}",
-                "npm install && npm update",
-                "npm run opse2e -- init",
-                "npm run opse2e -- protractor --configFile ${e2eSettings['configFile']} --baseUrl ${e2eSettings['baseUrl']} --branchName ${e2eSettings['contentBranch']} --noEscalation",
-                "",
-                "npm run opse2e -- reporter --src 'result/e2e/screenshots' --src '../${commonE2eRepoName}/result/e2e/screenshots' --dest 'result/e2e/screenshots'"
-                ].join('\n'))
+            shell(e2eSettings['buildSteps'].join('\n'))
         }
-        publishers {
-            archiveArtifacts {
-                pattern("${repoName}/result/e2e/screenshots/**/*.*") // Archive E2E test results
+        if (e2eSettings['archiveArtifacts']) {
+            publishers {
+                archiveArtifacts {
+                    pattern(e2eSettings['archiveArtifacts']) // Archive E2E test results
+                }
             }
         }
     }
@@ -186,11 +144,6 @@ def execute(settings) {
         release: 'master',
         hotfix: 'master'
     ]
-    def bumpMapping = [
-        develop: 'prerelease',
-        release: 'minor',
-        hotfix: 'patch'
-    ]
     branches.each {
         def ciJobName = folderName + '/' + it + '_ci'
         def opbuildJobName = folderName + '/' + it + '_opbuild'
@@ -198,36 +151,42 @@ def execute(settings) {
         def mergeJobName = folderName + '/' + it + '_merge'
         def versionJobName = folderName + '/' + it + '_bump_version'
 
-        def buildSteps = [
-            'npm install',
-            'npm update',
-            "npm run opst -- deployTheme -B docker_${it}"
-        ]
-        def bumpVersionSteps = [
-            'npm version ' + bumpMapping[it],
-            'git push --follow-tags ' + settings['ci']['repoUrl'] + ' HEAD:' + it
-        ]
+        def ciBranchMapping = settings['ci']['branchMapping']
+        def opbuildBranchMapping = settings['opbuild']['branchMapping']
         switch (it) {
             case "develop":
-                createJob(ciJobName, null, settings['ci'], it, buildSteps)
-                createOpbuildJob(opbuildJobName, it + '_ci', settings['opbuild'], it)
+                // create ci job
+                createJob(ciJobName, null, settings['ci'], it, ciBranchMapping[it]['buildSteps'])
+                // create opbuild job
+                createJob(opbuildJobName, it + '_ci', settings['opbuild'], it, opbuildBranchMapping[it]['buildSteps'])
+                // create e2e job
                 createE2eJob(e2eJobName, it + '_opbuild', settings['e2e'], it)
+                // create merge job
                 createMergeJob(mergeJobName, it + '_e2e', settings['ci'], it, mergeMapping[it])
                 break
             case "release":
-                createJob(ciJobName, null, settings['ci'], it, buildSteps)
-                createOpbuildJob(opbuildJobName, it + '_ci', settings['opbuild'], it)
+                // create ci job
+                createJob(ciJobName, null, settings['ci'], it, ciBranchMapping[it]['buildSteps'])
+                // create opbuild job
+                createJob(opbuildJobName, it + '_ci', settings['opbuild'], it, opbuildBranchMapping[it]['buildSteps'])
+                // create e2e job
                 createE2eJob(e2eJobName, it + '_opbuild', settings['e2e'], it)
-                createJob(versionJobName, null, settings['ci'], it, bumpVersionSteps) // bump version
+                // create bumpVersion job
+                createJob(versionJobName, null, settings['ci'], it, ciBranchMapping[it]['bumpVersionSteps'])
                 break
             case "hotfix":
-                createJob(ciJobName, null, settings['ci'], it, buildSteps)
-                createOpbuildJob(opbuildJobName, it + '_ci', settings['opbuild'], it)
+                // create ci job
+                createJob(ciJobName, null, settings['ci'], it, ciBranchMapping[it]['buildSteps'])
+                // create opbuild job
+                createJob(opbuildJobName, it + '_ci', settings['opbuild'], it, opbuildBranchMapping[it]['buildSteps'])
+                // create e2e job
                 createE2eJob(e2eJobName, it + '_opbuild', settings['e2e'], it)
-                createJob(versionJobName, null, settings['ci'], it, bumpVersionSteps) // bump version
+                // create bumpVersion job
+                createJob(versionJobName, null, settings['ci'], it, ciBranchMapping[it]['bumpVersionSteps'])
                 break
             case "master":
-                createJob(ciJobName, null, settings['ci'], it, buildSteps)
+                // create ci job
+                createJob(ciJobName, null, settings['ci'], it, ciBranchMapping[it]['buildSteps'])
                 // create master_mergeTo_develop & master_mergeTo_hotfix jobs
                 createMergeJob(mergeJobName + 'To_develop', null, settings['ci'], it, 'develop')
                 createMergeJob(mergeJobName + 'To_hotfix', null, settings['ci'], it, 'hotfix')
